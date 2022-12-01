fd841cec5f13c8a5cb3d4ca40aeedce68b2a87ed
cindy <boche.anaely@gmail.com>
2022-12-01 11:40:04 -0600

    cambios 1

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -2,7 +2,7 @@
 export const getPrueba = async (req, res) =>  {
   
    
-    const result = '{"name": "Eternal Flame","age": 1000000,"secretIdentity": "Unknown","powers": ["Immortality","Heat Immunity","Inferno","Teleportation","Interdimensional travel"]}"';
+    const result = '{"name": "DEV","age": 1000000,"secretIdentity": "Unknown","powers": ["Immortality","Heat Immunity","Inferno","Teleportation","Interdimensional travel"]}"';
     res.json(result);
   
   
